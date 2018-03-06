b00f8ec177dd4b649d4242fa0a8ad3d521e22875
clx <hbdennischen@outlook.com>
2018-03-06 17:22:44 +0800

    update version

--- index.js
+++ index.js
@@ -1,14 +1,34 @@
-const app = require('./tradeup'),
-    cluster = require('cluster'),
+const cluster = require('cluster'),
     os = require('os');
 
 var log4js = require('log4js');
-log4js.configure("log4js.conf", {reloadSecs: 300});
 var logger = log4js.getLogger();
 
-require('dotenv').config();
+module.exports = {
+    boot: function (app) {
+        if(process.env.DEVELOPMENT){
+            app();
+            return;
+        }
 
-if(process.env.DEVELOPMENT){
-    app();
-    return;
+        if (cluster.isMaster) {
+            var cpus = os.cpus().length;
+            for (var i = 0; i < cpus; i++) {
+                cluster.fork();
+                cluster.on('exit', function (worker, code) {
+                    if (code != 0 && !worker.suicide) {
+                        logger.info('Worker crashed. ~~~~~~~~~~~~~~~~~~~~~~~~~');
+                        /*logger.info('Worker crashed. Starting a new worker ~~~~~~~~~~~~~~~~~~~~~~~~~');
+                         cluster.fork();*/
+                    }
+                });
+            }
+        }
+        else {
+            app();
+        }
+    },
+    express: {
+        appBuilder: require('./express/AppBuilder')
+    }
 }
