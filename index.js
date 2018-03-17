a06e3e9c74102a623321374d380f9f2f34b456ae
clx1608@qq.com <hbdennischen@outlook.com>
2018-03-17 14:32:43 +0800

    取消boot(app)

--- index.js
+++ index.js
@@ -5,29 +5,6 @@ var log4js = require('log4js');
 var logger = log4js.getLogger();
 
 module.exports = {
-    boot: function (app) {
-        if (process.env.DEVELOPMENT) {
-            app();
-            return;
-        }
-
-        if (cluster.isMaster) {
-            var cpus = os.cpus().length;
-            for (var i = 0; i < cpus; i++) {
-                cluster.fork();
-                cluster.on('exit', function (worker, code) {
-                    if (code != 0 && !worker.suicide) {
-                        logger.info('Worker crashed. ~~~~~~~~~~~~~~~~~~~~~~~~~');
-                        /*logger.info('Worker crashed. Starting a new worker ~~~~~~~~~~~~~~~~~~~~~~~~~');
-                         cluster.fork();*/
-                    }
-                });
-            }
-        }
-        else {
-            app();
-        }
-    },
     express: {
         appBuilder: require('./express/AppBuilder'),
         handlebarsFactory: require('./express/HandlebarsFactory')
