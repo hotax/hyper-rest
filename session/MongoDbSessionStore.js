8f99008364b50a372b78edfdae9f597373937d93
clx1608@qq.com <hbdennischen@outlook.com>
2018-06-12 15:27:38 +0800

    Access passport.js user information from a socket.io connection

--- session/MongoDbSessionStore.js
+++ session/MongoDbSessionStore.js
@@ -1,5 +1,10 @@
 const session = require('express-session'),
     uuid = require('uuid-v4'),
+    cookieParser = require('cookie-parser'),
+    SocketIo = require('socket.io'),
+    PassportSocketIo = require('passport.socketio'),
+    KEY = 'express.sid',
+    SESSION_SECRET = process.env.SESSION_SECRET || 'super secret for session',
     logger = require('../app/Logger'),
     MongoDBStore = require('connect-mongodb-session')(session);
 
@@ -20,20 +25,31 @@ module.exports = function (maxAge) {
             // Use express session support since OAuth2orize requires it
             app.use(session({
                 genid: function () {
-                   return uuid();
+                    return uuid();
                 },
-                key: 'express.sid',
-                //cookie: {maxAge: 1000 * 60 * 60 * 24 * 7},// 1 week
-                //cookie: {maxAge: 1000 * 60 * 60 * 24},// 1 day
+                key: KEY,
                 cookie: {
                     maxAge: maxAge || 1000 * 60 * 60 * 24,
                     secure: process.env.NODE_ENV === 'production'
                 },
-                secret: process.env.SESSION_SECRET || 'super secret for session',
+                secret: SESSION_SECRET,
                 saveUninitialized: false,
                 resave: false,
                 store: store
             }));
+        },
+        authByServer: function (server) {
+            const io = SocketIo(server);
+            io.use(PassportSocketIo.authorize({
+                cookieParser: cookieParser,
+                key: KEY,
+                secret: SESSION_SECRET,
+                store: store,
+                success: function (data, accept) {
+                    logger.info('socket.io auth success');
+                    accept()
+                }
+            }))
         }
     };
 }
