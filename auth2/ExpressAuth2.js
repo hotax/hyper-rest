3d806f389d8aea82b731273cd74cb2381ff1d855
clx1608@qq.com <hbdennischen@outlook.com>
2018-10-15 16:05:59 +0800

    验证bearer Token

--- auth2/ExpressAuth2.js
+++ auth2/ExpressAuth2.js
@@ -2,7 +2,7 @@ const router = require('express').Router(),
     oAuth2Server = require('node-oauth2-server'),
     defaultGetUser = require('./db/MongoDbAuth2Schema').getUser;
 
-var __options;
+var __options, __app;
 
 /**
  *
@@ -100,6 +100,65 @@ function saveAccessToken(accessToken, clientID, expires, user, callback) {
     callback(null);
 }
 
+/**
+ * This method is called to validate a user when they're calling APIs
+ * that have been authenticated. The user is validated by verifying the
+ * bearerToken which must be supplied when calling an endpoint that requires 
+ * you to have been authenticated. We can tell if a  bearer token is valid 
+ * if passing it to the getUserIDFromBearerToken() method returns a userID. 
+ * It's able to return a userID because each row in the access_tokens table 
+ * has a userID in itbecause we store it along with the userID when we save
+ * the bearer token. So, as they're bothpresent in the same row in the db we
+ * should be able to use the bearerToken to query for a row  which will have
+ a userID in it.
+ *
+ * @param bearerToken
+ * @param callback
+ * The callback takes 2 parameters:
+ *
+ * 1. A truthy boolean indicating whether or not an error has occured. It
+ should be set to a truthy if there is an error or a falsy if there is no
+ error
+ *
+ * 2. An accessToken which will be accessible in the  req.user object on 
+ * endpoints that are authenticates
+ */
+function getAccessToken(bearerToken, callback) {
+    var accessToken = function () {
+        return Promise.reject('getAccessToken function is not defined!');
+    }
+    if (__options && __options.model && __options.model.getAccessToken) {
+        accessToken = __options.model.getAccessToken;
+    }
+    return accessToken(bearerToken)
+        .then(function (token) {
+            return callback(false, token);
+        })
+        .catch(function (error) {
+            return callback(error);
+        })
+}
+
+/**
+ * Creates and returns an accessToken which contains an expiration date field.
+ * You can assign null to it to ensure the token doesn't expire.  You must also 
+ * assign it either a user object, or a userId whose value must be a string or
+ * number. If you create a user object you can access it in authenticated endpoints
+ * in the req.user variable. If you create a userId you can access it in authenticated 
+ * endpoints in the req.user.id variable.
+ *
+ * @param userID
+ * @returns {Promise.<{user: {id: *}, expires: null}>}
+ */
+function createAccessTokenFrom(userID) {
+    return Promise.resolve({
+        user: {
+            id: userID,
+        },
+        expires: null
+    })
+}
+
 module.exports = function (options) {
     __options = options;
     const oAuth = oAuth2Server({
@@ -107,7 +166,8 @@ module.exports = function (options) {
             getClient: getClient,
             grantTypeAllowed: grantTypeAllowed,
             getUser: getUser,
-            saveAccessToken: saveAccessToken
+            saveAccessToken: saveAccessToken,
+            getAccessToken: getAccessToken
         },
         grants: ['password'],
     });
@@ -117,7 +177,11 @@ module.exports = function (options) {
     }
 
     return {
+        authorize: function (method, uri, handler) {
+            __app[method](uri, oAuth.authorise(), handler);
+        },
         attachTo: function (app) {
+            __app = app;
             app.oauth = oAuth;
             createLoginRoute();
             app.use('/auth', router);
