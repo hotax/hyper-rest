2442f5108b8b2d584fc30e2c5ccff963213c0ff1
clx <hbdennischen@gmail.com>
2021-05-27 15:35:03 +0800

    assure all call to entity must be with instance

--- db/mongoDb/DbEntity.js
+++ db/mongoDb/DbEntity.js
@@ -1,5 +1,5 @@
 const __ = require('underscore'),
-{each, findIndex, initial, extend, last, isString, isUndefined} = __,
+{each, map, pick, findIndex, initial, extend, last, isString, isUndefined} = __,
 createSubdocConfig = require('./CreateSubdocConfig')
 
 function __getUpdatedAtNameFromSchema(schema) {
@@ -60,8 +60,7 @@ const __findSubDocFromParent = (doc, subDocId, path, filter) => {
 }
 
 class Entity {
-    constructor(config, addIn) {
-        extend(this, addIn)
+    constructor(config) {
         this.__config = config
         this.__subdoc = createSubdocConfig(config.subdoc)
     }
@@ -281,6 +280,65 @@ class Entity {
     }
 }
 
-module.exports = (config, addIn) => {
-    return new Entity(config, addIn)
+const __create = (config, addIn) => {
+    const entity = new Entity(config)
+    const obj = {
+        create(data) {
+            return entity.create(data)
+        },
+
+        findById(id, projection) {
+            return entity.findById(id, projection)
+        },
+        
+        findBySubDocId(subDocId, paths) {
+            return entity.findBySubDocId(subDocId, paths)
+        },
+
+        findSubDocById(id, path) {
+            return entity.findSubDocById(id, path)
+        },
+
+        search(cond, text, sort) {
+            return entity.search(cond, text, sort)
+        },
+
+        ifMatch(id, version) {
+            return entity.ifMatch(id, version)
+        },
+
+        ifUnmodifiedSince(id, version) {
+            return entity.ifUnmodifiedSince(id, version)
+        },
+
+        update(data) {
+            return entity.update(data)
+        },
+
+        updateSubDoc(path, data) {
+            return entity.updateSubDoc(path, data)
+        },
+
+        remove(id) {
+            return entity.remove(id)
+        },
+
+        removeSubDoc(id, path) {
+            return entity.removeSubDoc(id, path)
+        },
+
+        listSubs(parentId, path) {
+            return entity.listSubs(parentId, path)
+        },
+
+        createSubDoc(parentId, path, data) {
+            return entity.createSubDoc(parentId, path, data)
+        },
+
+        ...addIn
+    }
+
+    return obj
 }
+
+module.exports = __create
