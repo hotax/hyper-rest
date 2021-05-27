8e79cce3c66d874fb5ca100dabd233c1e704f638
clx <hbdennischen@gmail.com>
2021-05-27 13:41:21 +0800

    refact create DbEntity again

--- db/mongoDb/DbEntity.js
+++ db/mongoDb/DbEntity.js
@@ -1,5 +1,5 @@
 const __ = require('underscore'),
-{each, map, pick, findIndex, initial, extend, last, isString, isUndefined} = __,
+{each, findIndex, initial, extend, last, isString, isUndefined} = __,
 createSubdocConfig = require('./CreateSubdocConfig')
 
 function __getUpdatedAtNameFromSchema(schema) {
@@ -60,7 +60,8 @@ const __findSubDocFromParent = (doc, subDocId, path, filter) => {
 }
 
 class Entity {
-    constructor(config) {
+    constructor(config, addIn) {
+        extend(this, addIn)
         this.__config = config
         this.__subdoc = createSubdocConfig(config.subdoc)
     }
@@ -280,61 +281,6 @@ class Entity {
     }
 }
 
-const __create = (config, addIn) => {
-    const entity = new Entity(config)
-    const obj = {
-        create(data) {
-            return entity.create(data)
-        },
-
-        findById(id, projection) {
-            return entity.findById(id, projection)
-        },
-
-        findSubDocById(id, path) {
-            return entity.findSubDocById(id, path)
-        },
-
-        search(cond, text, sort) {
-            return entity.search(cond, text, sort)
-        },
-
-        ifMatch(id, version) {
-            return entity.ifMatch(id, version)
-        },
-
-        ifUnmodifiedSince(id, version) {
-            return entity.ifUnmodifiedSince(id, version)
-        },
-
-        update(data) {
-            return entity.update(data)
-        },
-
-        updateSubDoc(path, data) {
-            return entity.updateSubDoc(path, data)
-        },
-
-        remove(id) {
-            return entity.remove(id)
-        },
-
-        removeSubDoc(id, path) {
-            return entity.removeSubDoc(id, path)
-        },
-
-        listSubs(parentId, path) {
-            return entity.listSubs(parentId, path)
-        },
-
-        createSubDoc(parentId, path, data) {
-            return entity.createSubDoc(parentId, path, data)
-        },
-
-        ...addIn
-    }
-
-    return obj
+module.exports = (config, addIn) => {
+    return new Entity(config, addIn)
 }
-
-module.exports = __create
